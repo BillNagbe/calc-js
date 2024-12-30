dbfbc561b774732e287c211ca43595f499497d26
Bill Nagbe <BillyNagbe@gmail.com>
2024-12-29 22:26:29 -0600

    calc functionality added

--- main.js
+++ main.js
@@ -2,8 +2,19 @@
 
 let num = 0;
 
-while(num < 3) {
+/* while(num < 3) {
     console.log(num);
     num++;
 }
+*/
+const calc = {
+    add(x, y) {
+        return x + y;
+    },
 
+    subject(x, y) {
+        return x - y;
+    }
+}
+
+console.log(calc.add(2, 5));
